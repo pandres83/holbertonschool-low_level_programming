ab82f8ee38f8aff55ae67e00f7451df1e29ead66
pandres83 <andrespineda0209@gmail.com>
2023-02-24 14:15:28 -0800

    fix 4rd

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: 'the program's description'
+ * main - Entry point * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("sandra");
 	return (0);
 }
