727817a8cadc6057e4f9400e35870b20e36fe5fe
pandres83 <andrespineda0209@gmail.com>
2023-02-27 07:39:52 -0800

    Variables

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -8,12 +8,7 @@ int main(void)
 {
 	char lc;
 
-	for (lc = 'a'; lc <= 'z'; lc++)
-	{
-		putchar(lc);
-	}
-
-	for (lc = 'A'; lc <= 'z'; lc++)
+	for (lc = 'z'; lc >= 'a'; lc--)
 	{
 		putchar(lc);
 	}
