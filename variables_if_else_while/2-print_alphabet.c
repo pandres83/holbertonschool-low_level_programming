70f0314ba95affee45b5f32d685d4d66bdb35eb9
pandres83 <andrespineda0209@gmail.com>
2023-02-26 17:20:24 -0800

    Variables

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *Excercise two - project: C - Variables, if, else, while
+ *Description: prints the alphabet in lower case
+ */
+int main(void)
+{
+	char lc;
+	for(lc='a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return(0);
+}
