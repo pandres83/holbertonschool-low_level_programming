93c3d04511b6ea7e3b87db5984d844b891ce665e
pandres83 <andrespineda0209@gmail.com>
2023-02-26 16:09:45 -0800

    Variables

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * Ejercicio last_digit
+ * Description: Este ejercicio obtiene el ultimo numero entero de una cadena de numeros 
+*/
+
 int main(void)
 {
 	int n;
@@ -13,16 +16,14 @@ int main(void)
 	last_digit = n % 10;
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",n,last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
-		else if (last_digit==0)
+		else if (last_digit == 0)
 		{
-			printf("Last digit of %d is %d and is 0\n",n,last_digit);
+			printf("Last digit of %d is %d and is 0\n", n, last_digit);
 		}else
 		{
-			printf("Last digit of %d is %d and is less than 6 and not 6\n",n,last_digit);
+			printf("Last digit of %d is %d and is less than 6 and not 6\n", n, last_digit);
 		}
-
-     
 	return (0);
 }
