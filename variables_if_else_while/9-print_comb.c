243afa9f9ab9b31e15b0529da51174482873fda8
pandres83 <andrespineda0209@gmail.com>
2023-02-27 08:21:03 -0800

    Variables

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *Excercise nine - project: C - Variables, if, else, while
+ *Description: Prints all possible combinations of single digit number
+ *Return: Always (0);
+ */
+int main(void)
+{
+	int number;
+	for (number = 0; number < 9; number++)
+	{
+		putchar(number + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar(9 + '0');
+	return (0);
+}
