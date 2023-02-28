4cac3916d008caebc204e4b2c7a6bcc6e38a1fd9
pandres83 <andrespineda0209@gmail.com>
2023-02-28 09:51:29 -0800

    Variables

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main: it print the word _putchar
+ */
+
+int main(void)
+{
+        putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	puts("\n");
+	return(0);
+}
