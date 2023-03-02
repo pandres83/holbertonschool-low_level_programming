141c7a2eeeaa303baec5e3e31c93f865ea772f7d
pandres83 <andrespineda0209@gmail.com>
2023-03-02 08:10:45 -0800

    Variables

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 9; number++)
+	for (number = 0; number <= 9; number++)
 	{
 		putchar(number + '0');
 		putchar(',');
 		putchar(' ');
 	}
-	putchar(9 + '0');
+	putchar('\n');
 	return (0);
 }
