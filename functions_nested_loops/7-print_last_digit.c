7376dabceadc2e640e02439cdea4196b38fa7b27
pandres83 <andrespineda0209@gmail.com>
2023-03-04 06:41:19 -0800

    Variables

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  *print_last_digit - print last digit
  *@c: This parameter receive a caracter
@@ -8,11 +7,18 @@
  *Description: prints lowercase caractwer
  *Return: integer
  */
-void print_last_digit(int c)
+int print_last_digit(int c)
 {
 	int answer;
 
-	answer = c % 10;
-	putchar(answer);
-	return;
+	if (c < 0)
+	{
+		answer = (c * -1) % 10;
+	}
+	else
+	{
+		answer =c % 10;
+	}
+	printf("%d", answer);
+	return (answer);
 }
