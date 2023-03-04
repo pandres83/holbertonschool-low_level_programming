95025ba0eb069ba46768b8957e7b6e6e4899bd78
pandres83 <andrespineda0209@gmail.com>
2023-03-04 13:24:28 -0800

    Variables

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ *reset_to_98 - this algorithm changes the value oa variable for 98
+ *@n: pointer
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*Declare a function that prints the word _putchar*/
+int _putchar(char c);
+/*function that prints the alphabet*/
+void print_alphabet(void);
+/* Function that prints 10 times the alphabet*/
+void print_alphabet_x10(void);
+/*Function that*/
+int _islower(int c);
+/*Validate wheter a character is number is aalphabetic or not*/
+int _isalpha(int c);
+/*Print abs number*/
+int _abs(int);
+/*Print  the last digit*/
+int print_last_digit(int);
+/*times table exercise*/
+void times_table(void);
+/*print numbers*/
+void print_to_98(int n);
+/*plus two number*/
+int add(int, int);
+/*pointer*/
+void reset_to_98(int *n);
