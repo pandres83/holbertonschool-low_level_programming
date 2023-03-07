e1c1992a5d880c8f3966ee9732704e7f0f0dfd66
pandres83 <andrespineda0209@gmail.com>
2023-03-07 05:28:58 -0800

    Variables

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <string.h>
 /**
- *_strcpy - copy a string of character
- *src: source
- *@dest:destiny
+ **_strcpy - copy a string of character
+ *@dest: Destination
+ *@src: Source
  *Description: copy a string
+ *Return: A string
 */
 char *_strcpy(char *dest, char *src)
 {
 	char *copy;
 
 	copy = strcpy(dest, src);
-	return(copy);
+	return (copy);
 }
